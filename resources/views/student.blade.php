cca1eaff5c493f0904a865cf3cf7056e0eb93c38
= <=>
2017-03-20 20:33:30 +0700

    Edit Delete page

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -29,7 +29,7 @@
               <td>{{ $student->student_tel }}</td>
               <td>
                 <a href="" class="btn btn-info btn-sm">Edit</a>
-                <a href="/delete/{{ $student->student_id }}" class="btn btn-danger btn-sm">Delete</a>
+                <a href="/delete/{{ $student->student_id }}" class="btn btn-danger btn-sm btn-delete">Delete</a>
               </td>
             </tr>
             @endforeach
@@ -45,5 +45,11 @@
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <script>
+      
+      $('.btn-delete').click(function(){
+        return confirm('Are u sure want to delete');
+      });
+    </script>
   </body>
 </html>
